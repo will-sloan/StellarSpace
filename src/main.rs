b7a865458a670b3beb3c7b7bca951c59412700f6
Will Sloan <willis5552@gmail.com>
2020-08-23 08:53:24 -0400

    as plugins

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,14 @@
-use bevy::prelude::*;
-use bevy::sprite::collide_aabb::{collide, Collision};
-
-fn main() {
-    App::build()
-        .add_default_plugins()
-        .add_startup_system(setup.system())
-        .add_system(animate_sprite_system.system())
-        .add_system(collision_system.system())
-        .run();
-}
-struct MainDude {
-    speed: f32,
-}
+use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    input::mouse::MouseButtonInput,
+    prelude::*,
+    render::pass::ClearColor,
+    sprite::collide_aabb::{collide, Collision},
+};
 
 struct Solid {}
-const GUYSPEED: f32 = 20.0;
-const SCALE_FACTOR: f32 = 3.0;
-const SPRITEDIMENSIONS: f32 = 16.0;
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut textures: ResMut<Assets<Texture>>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    windows: Res<Windows>,
-) {
-    let texture_handle = asset_server
-        .load_sync(&mut textures, "assets/GIMP FIGURES/char_1-Sheet.png")
-        .unwrap();
-    let texture = textures.get(&texture_handle).unwrap();
-
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 6, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    // Camera and Main Character
-    commands
-        .spawn(Camera2dComponents::default())
-        // .spawn(SpriteComponents {
-        //     material: materials.add(Color::rgb(0.66, 0.206, 0.245).into()),
-        //     sprite: Sprite {
-        //         size: Vec2::new(1620.0, 720.0),
-        //     },
-        //     translation: Translation(Vec3::new(0.0, 0.0, 0.0)),
-        //     ..Default::default()
-        // })
-        .spawn(SpriteSheetComponents {
-            texture_atlas: texture_atlas_handle,
-            translation: Translation(Vec3::new(-400.0, -400.0, 0.1)),
-            scale: Scale(SCALE_FACTOR),
-            ..Default::default()
-        })
-        .with(Timer::from_seconds(0.05))
-        .with(MainDude { speed: GUYSPEED });
-
-    // commands
-    //     .spawn(Camera2dComponents::default())
-    //     .spawn(SpriteComponents {
-    //         material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
-    //         translation: Translation(Vec3::new(0.0, 0.0, 0.1)),
-    //         sprite: Sprite {
-    //             size: Vec2::new(30.0, 30.0),
-    //         },
-    //         ..Default::default()
-    //     })
-    //     .with(Timer::from_seconds(0.05))
-    //     .with(MainDude { speed: GUYSPEED });
-
+fn map_setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands
         .spawn(SpriteComponents {
             material: materials.add(Color::rgb(1.8, 1.2, 1.2).into()),
@@ -78,30 +20,131 @@ fn setup(
         })
         .with(Solid {});
 
+    // Future walls
+    // commands
+    //     .spawn(SpriteComponents {
+    //         material: materials.add(Color::rgb(1.8, 1.2, 1.2).into()),
+    //         translation: Translation(Vec3::new(0.0, -50.0, 0.2)),
+    //         sprite: Sprite {
+    //             size: Vec2::new(300.0, 300.0),
+    //         },
+    //         ..Default::default()
+    //     })
+    //     .with(Solid {});
+
+    // commands
+    //     .spawn(SpriteComponents {
+    //         material: materials.add(Color::rgb(1.8, 1.2, 1.2).into()),
+    //         translation: Translation(Vec3::new(0.0, 0.0, 0.2)),
+    //         sprite: Sprite {
+    //             size: Vec2::new(20.0, 300.0),
+    //         },
+    //         ..Default::default()
+    //     })
+    //     .with(Solid {});
+
     // let window = match windows.get_primary() {
     //     Some(a) => a,
     //     None => panic!("No Window!!! How is this Possible!!!"),
     // };
+}
 
-    // Platforms
-    // commands.spawn(SpriteComponents {
-    //     material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
-    //     sprite: Sprite {
-    //         size: Vec2::new(20.0, 20.0),
-    //     },
-    //     translation: Translation(Vec3::new(0.0, -160.0, 0.0)),
-    //     ..Default::default()
-    // });
+pub struct MapPlugin;
 
-    // // Background ?
-    commands.spawn(SpriteComponents {
-        material: materials.add(Color::rgb(0.66, 0.206, 0.245).into()),
-        sprite: Sprite {
-            size: Vec2::new(1620.0, 720.0),
-        },
-        translation: Translation(Vec3::new(0.0, 0.0, 0.0)),
-        ..Default::default()
-    });
+impl Plugin for MapPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(map_setup.system());
+    }
+}
+
+fn text_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font_handle = asset_server.load("FiraSans-Bold.ttf").unwrap();
+    commands
+        // texture
+        .spawn(TextComponents {
+            style: Style {
+                align_self: AlignSelf::FlexEnd,
+                ..Default::default()
+            },
+            text: Text {
+                value: "FPS:".to_string(),
+                font: font_handle,
+                style: TextStyle {
+                    font_size: 60.0,
+                    color: Color::BLUE,
+                },
+            },
+            ..Default::default()
+        });
+}
+
+fn text_display_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text>) {
+    for mut text in &mut query.iter() {
+        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(average) = fps.average() {
+                text.value = format!("FPS: {:.2}", average);
+            }
+        }
+    }
+}
+
+pub struct FPSTextPlugin;
+
+impl Plugin for FPSTextPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_plugin(FrameTimeDiagnosticsPlugin::default())
+            .add_startup_system(text_setup.system())
+            .add_system(text_display_system.system());
+    }
+}
+
+// fn text_update_system() {
+
+// }
+
+struct MainDude {
+    speed: f32,
+}
+const GUYSPEED: f32 = 20.0;
+const SCALE_FACTOR: f32 = 3.0;
+const GUYHEIGHT: f32 = 14.0;
+const GUYWIDTH: f32 = 10.0;
+
+pub struct CharacterPlugin;
+
+impl Plugin for CharacterPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(char_setup.system())
+            .add_system(animate_sprite_system.system())
+            .add_system(char_collision_system.system())
+            .add_system(position_mouse_click_system.system());
+    }
+}
+
+fn char_setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut textures: ResMut<Assets<Texture>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server
+        .load_sync(&mut textures, "assets/GIMP FIGURES/char_1-Sheet.png")
+        .unwrap();
+    let texture = textures.get(&texture_handle).unwrap();
+
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 6, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    // Camera and Main Character
+    commands
+        .spawn(SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle,
+            translation: Translation(Vec3::new(-300.0, -300.0, 0.1)),
+            scale: Scale(SCALE_FACTOR),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(0.05, true))
+        .with(MainDude { speed: GUYSPEED });
 }
 
 fn animate_sprite_system(
@@ -114,64 +157,13 @@ fn animate_sprite_system(
         &MainDude,
     )>,
 ) {
-    for (mut timer, mut _sprite, mut translation, maindude) in &mut query.iter() {
-        //for (mut timer, mut translation, maindude) in &mut query.iter() {
+    for (timer, mut _sprite, mut translation, maindude) in &mut query.iter() {
         if timer.finished {
-            let window = match windows.get_primary() {
+            let _window = match windows.get_primary() {
                 Some(a) => a,
                 None => panic!("No Window!!! How is this Possible!!!"),
             };
 
-            // println!("{}", sprite.draw.is_transparent);
-            // Easy way to cover up old code
-            // {
-            /*
-            // if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
-            //     sprite.index = match sprite.index {
-            //         0 => 1,
-            //         1 => 2,
-            //         2 => 1,
-            //         _ => 0,
-            //     };
-            //     if sprite.index == 1 || sprite.index == 2 {
-            //         *translation.x_mut() -= maindude.speed;
-            //     }
-            // }
-            // if keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right) {
-            //     sprite.index = match sprite.index {
-            //         5 => 3,
-            //         3 => 4,
-            //         4 => 3,
-            //         _ => 5,
-            //     };
-            //     if sprite.index == 3 || sprite.index == 4 {
-            //         *translation.x_mut() += maindude.speed;
-            //     }
-            // }
-            // if keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up) {
-            //     sprite.index = match sprite.index {
-            //         5 => 3,
-            //         3 => 4,
-            //         4 => 3,
-            //         _ => 5,
-            //     };
-            //     if sprite.index == 3 || sprite.index == 4 {
-            //         *translation.y_mut() += maindude.speed;
-            //     }
-            // }
-            // if keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down) {
-            //     sprite.index = match sprite.index {
-            //         5 => 3,
-            //         3 => 4,
-            //         4 => 3,
-            //         _ => 5,
-            //     };
-            //     if sprite.index == 3 || sprite.index == 4 {
-            //         *translation.y_mut() -= maindude.speed;
-            //     }
-            // }
-             */
-            // }
             if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
                 *translation.x_mut() -= maindude.speed;
                 print!("x: {}", translation.x_mut());
@@ -192,10 +184,6 @@ fn animate_sprite_system(
                 print!("x: {}", translation.x_mut());
                 println!(" y: {}", &translation.y_mut());
             }
-            // let right_side =
-            // let left_side =
-            // let top =
-            // let bottom =
 
             // boundaries
             // *translation.0.x_mut() = f32::max(
@@ -213,147 +201,122 @@ fn animate_sprite_system(
             //     ),
             // );
 
-            // print!("x: {}", translation.x_mut());
-            // println!(" y: {}", &translation.y_mut());
-            // else {
-            //     sprite.index = match sprite.index {
-            //         0..=2 => 0,
-            //         _ => 5,
-            //     }
-            // }
-            timer.reset();
+            //timer.reset();
         }
     }
 }
 
-fn collision_system(
+fn char_collision_system(
     mut query_player: Query<(&MainDude, &mut TextureAtlasSprite, &mut Translation)>,
     mut query_walls: Query<(&Solid, &Sprite, &mut Translation)>,
 ) {
-    // println!("Hello");
-    for (_, char_sprite, mut char_loc) in &mut query_player.iter() {
-        //println!("1");
+    for (_, _char_sprite, mut char_loc) in &mut query_player.iter() {
         for (_, wall_sprite, wall_loc) in &mut query_walls.iter() {
-            // println!(
-            //     "a_pos: {}, a_size: {}, b_pos: {}, b_size: {}",
-            //     char_loc.0,
-            //     Vec2::new(SPRITEDIMENSIONS, SPRITEDIMENSIONS),
-            //     wall_loc.0,
-            //     wall_sprite.size,
-            // );
             let collision = collide(
                 char_loc.0,
-                Vec2::new(SPRITEDIMENSIONS, SPRITEDIMENSIONS),
+                Vec2::new(GUYWIDTH * SCALE_FACTOR, GUYHEIGHT * SCALE_FACTOR),
                 wall_loc.0,
                 wall_sprite.size,
             );
-            // println!(
-            //     "{} {} {} {}",
-            //     char_loc.0,
-            //     Vec2::new(SPRITEDIMENSIONS, SPRITEDIMENSIONS),
-            //     wall_loc.0,
-            //     wall_sprite.size
-            // );
-            //println!("{}", char_loc.0.x());
-            /*
-                if (rect1.x < rect2.x + rect2.width &&
 
-                rect1.x + rect1.width > rect2.x &&
-
-                rect1.y < rect2.y + rect2.height &&
-
-                rect1.y + rect1.height > rect2.y) {
-                    // collision detected!
-                }
-            */
-            /*
-                rect1.x =  char_loc.0.x()
-                rect1.y = char_loc.0.y()
-                rect1.width && rect1.height = SPRITEDIMENSIONS
-
-                rect2.x = wall_loc.0.x()
-                rect2.y = wall_loc.0.y()
-                rect2.width = wall_sprite.size.x()
-                rect2.height = wall_sprite.size.y()
-
-                if (char_loc.0.x() < wall_loc.0.x() + wall_sprite.size.x() &&
-
-                char_loc.0.x() + SPRITEDIMENSIONS > wall_loc.0.x() &&
-
-                char_loc.0.y() < wall_loc.0.y() + wall_sprite.size.y() &&
-
-                char_loc.0.y() + SPRITEDIMENSIONS > wall_loc.0.y()) {
-                    // collision detected!
-                }
-
-            */
-            // if char_loc.0.x() < wall_loc.0.x() + wall_sprite.size.x()
-            //     && char_loc.0.x() + SPRITEDIMENSIONS > wall_loc.0.x()
-            //     && char_loc.0.y() < wall_loc.0.y() + wall_sprite.size.y()
-            //     && char_loc.0.y() + SPRITEDIMENSIONS > wall_loc.0.y()
-            // {
-            //     println!("\n\n\nHIT\n\n\n");
-            // }
-
-            // if char_loc.0.x() < wall_loc.0.x() + wall_sprite.size.x()
-            //     && char_loc.0.x() + SPRITEDIMENSIONS > wall_loc.0.x()
-            //     && char_loc.0.y() < wall_loc.0.y() + wall_sprite.size.y()
-            //     && char_loc.0.y() + SPRITEDIMENSIONS > wall_loc.0.y()
-            // {
-            //     println!("\n\n\nHIT\n\n\n");
-            // }
-
-            //if (char_loc.0)
             if let Some(collision) = collision {
-                //println!("{:?}", collision);
+                println!("{:?}", collision);
                 match collision {
                     Collision::Left => {
-                        println!(
-                            "LEFT {} {} {}",
-                            char_loc.x_mut(),
-                            wall_loc.0.x(),
-                            SPRITEDIMENSIONS
-                        );
-                        //println!("Hit left");
-                        *char_loc.x_mut() = wall_loc.0.x() - SPRITEDIMENSIONS; //f32::min(wall_loc.0.x() - SPRITEDIMENSIONS, )*char_loc.x_mut() - //SPRITEDIMENSIONS;
-                                                                               //wall_sprite.size.x();
+                        *char_loc.x_mut() = wall_loc.0.x() - wall_sprite.size.x() / 2.0 - GUYWIDTH;
                     }
                     Collision::Right => {
-                        println!(
-                            "RIGHT {} {} {}",
-                            char_loc.x_mut(),
-                            wall_loc.0.x(),
-                            SPRITEDIMENSIONS
-                        );
-                        //println!("Hit right");
-                        *char_loc.x_mut() = wall_loc.0.x() + SPRITEDIMENSIONS; //*char_loc.x_mut() + //SPRITEDIMENSIONS;
-                                                                               //wall_sprite.size.x()
+                        *char_loc.x_mut() = wall_loc.0.x() + wall_sprite.size.x() / 2.0 + GUYWIDTH;
                     }
                     Collision::Top => {
-                        println!(
-                            "TOP {} {} {}",
-                            char_loc.y_mut(),
-                            wall_loc.0.y(),
-                            SPRITEDIMENSIONS
-                        );
-                        *char_loc.y_mut() = wall_loc.0.y() +//*char_loc.y_mut() + //wall_sprite.size.y();
-                     SPRITEDIMENSIONS + wall_sprite.size.y()/2.0;
+                        *char_loc.y_mut() = wall_loc.0.y() + GUYHEIGHT + wall_sprite.size.y() / 2.0;
                     }
                     Collision::Bottom => {
-                        println!(
-                            "BOTTOM Char Y {} Wall Loc {:?} SPRITE {} CALC {}",
-                            char_loc.y_mut(),
-                            wall_loc.0,
-                            SPRITEDIMENSIONS,
-                            wall_loc.0.y() - wall_sprite.size.y() / 2.0 - SPRITEDIMENSIONS
-                        );
-                        *char_loc.y_mut() =
-                            wall_loc.0.y() - wall_sprite.size.y() / 2.0 - SPRITEDIMENSIONS
-                        //*char_loc.y_mut() - SPRITEDIMENSIONS;
-                        //wall_sprite.size.y();
+                        *char_loc.y_mut() = wall_loc.0.y() - wall_sprite.size.y() / 2.0 - GUYHEIGHT
                     }
                 }
             }
         }
     }
+}
+
+fn position_mouse_click_system(
+    mut state: ResMut<State>,
+    mouse_pos: ResMut<MouseLoc>,
+    mouse_button_input_events: Res<Events<MouseButtonInput>>,
+    //cursor_moved_events: Res<Events<CursorMoved>>,
+    // cursor_movement: Res<Events<CursorMoved>>,
+    // mouse_button: Res<Events<MouseButtonInput>>,
+    // mut query_player: Query<(&MainDude, &mut TextureAtlasSprite, &mut Translation)>,
+) {
+    // println!("{:?}", cursor_movement);
+    // println!("{:?}", mouse_button);
+    //println!("Hello");
+    // for (cursor_event, mouse_event) in state
+    //     .cursor_moved_event_reader
+    //     .iter(&cursor_moved_events)
+    //     .zip(
+    //         state
+    //             .mouse_button_event_reader
+    //             .iter(&mouse_button_input_events),
+    //     )
+    // {
+    //     println!("{:?}", cursor_event);
+    //     println!("{:?}", mouse_event);
+    // }
+    // for event in state.cursor_moved_event_reader.iter(&cursor_moved_events) {
+    //     mouse_pos.0 = event.position;
+    // }
+
+    for event in state
+        .mouse_button_event_reader
+        .iter(&mouse_button_input_events)
+    {
+        println!("event: {:?} position: {:?}", event, mouse_pos.0);
+    }
+}
+#[derive(Default)]
+struct State {
+    mouse_button_event_reader: EventReader<MouseButtonInput>,
+    cursor_moved_event_reader: EventReader<CursorMoved>,
+}
+
+struct MouseLoc(Vec2);
+
+fn mouse_movement_updating_system(
+    mut mouse_pos: ResMut<MouseLoc>,
+    mut state: ResMut<State>,
+    cursor_moved_events: Res<Events<CursorMoved>>,
+) {
+    for event in state.cursor_moved_event_reader.iter(&cursor_moved_events) {
+        //println!("Hello");
+        mouse_pos.0 = event.position;
+    }
+}
+
+fn general_setup(mut commands: Commands) {
+    //commands.spawn(UiCameraComponents::default());
+    commands.spawn(Camera2dComponents::default());
+}
+
+pub struct GeneralPlugin;
+
+impl Plugin for GeneralPlugin {
+    // The Camera, Background,and Mouse Position Stuff
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(general_setup.system())
+            .add_resource(ClearColor(Color::rgb(0.5, 0.20, 0.80)))
+            .init_resource::<State>()
+            .add_resource(MouseLoc(Vec2::new(0.0, 0.0)))
+            .add_system(mouse_movement_updating_system.system());
+    }
+}
+
+fn main() {
+    App::build()
+        .add_default_plugins()
+        .add_plugin(GeneralPlugin)
+        .add_plugin(CharacterPlugin)
+        .add_plugin(MapPlugin)
+        .run();
 }
